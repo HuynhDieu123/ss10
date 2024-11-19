2bff1f7c3438faba67b2d192c3ae299aeb6fac71
Dieu <cdinh1078@gmail.com>
2024-11-19 08:21:58 +0700

    edit_config

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://dieu:15122005@cluster0.1xns1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
